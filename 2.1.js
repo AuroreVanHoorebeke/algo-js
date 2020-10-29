fb6a519b893b12544239dca879a02f558a0d8eae
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-10-29 11:25:18 +0100

    exercise 2.1

---
+++ 2.1.js
@@ -0,0 +1,19 @@
+/*Web version with alerts
+
+let age = prompt("What is you age?");
+
+if (age >= 18) {
+    alert("You are an adult");
+} else (age < 18); {
+    alert("You are a child");
+} */
+
+/*Web console version with console.log*/
+
+let age = prompt("What is you age?");
+
+if (age >= 18) {
+    console.log("You are an adult");
+} else (age < 18); {
+    console.log("You are a child");
+}
