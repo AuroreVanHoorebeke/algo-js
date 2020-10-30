acd9f057d525cc61b9d1b7596a3ecc6da4b75fb7
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-10-30 09:50:17 +0100

    cleaned 1.1.js and created 2.2.js and .gitignore

---
+++ 2.2.js
@@ -0,0 +1,24 @@
+// Former version, using prompt instead of readlineSync
+// let min = Number(prompt("Enter a number :"));
+// let max = Number(prompt("Enter a bigger number :"));
+// let current = Number(prompt("Enter another number :"));
+
+/*Version with readlineSync*/
+const readlineSync = require('readline-sync')
+
+let min = Number(readlineSync.question("Enter a number: "));
+let max = Number(readlineSync.question("Enter a bigger number: "));
+
+if (min >= max) {
+    console.log("Looks like you don't understand what \"BIGGER\" means... Goodbye!");
+
+} else {
+    let current = Number(readlineSync.question("Enter another number: "))
+    if (current > min && current < max) {
+        console.log(current + " is between " + min + " and " + max + ".");
+    } else {
+        console.log(current + " isn't between " + min + " and " + max + ".");
+    }
+}
+
+}
