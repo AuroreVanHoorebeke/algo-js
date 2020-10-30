08647775a882fb9c97120cd7c8bcd168677ac1b7
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-10-30 10:45:27 +0100

    exercise 2.3

---
+++ 2.3.js
@@ -0,0 +1,15 @@
+// // While version - Should execute 100 times BUT display only the even numbers.
+
+// let i = 0
+// while ( i <= 100 ) {
+//     i ++;
+//     if ( i%2==0) {
+//         console.log(i)
+//     }
+// }
+
+ // For version - Should execute 50 times to display even numbers.
+
+ for (let i = 0; i <= 100; i += 2){
+    console.log(i)
+ }
