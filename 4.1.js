daae0fe08ec4862daf8e8d2f258d78a6268a28fd
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-06 16:17:19 +0100

    Exercise 4.1

---
+++ 4.1.js
@@ -0,0 +1,13 @@
+//Aim: create a program with a  "calcSurface" function, returning the surface of a rectangle according to length and width given by the user.
+
+const readlineSync = require('readline-sync')
+
+let l = readlineSync.question("Give the length of your rectangle: ");
+let w = readlineSync.question("Give the width of your rectangle: ");
+
+// calcSurface is a function taking as arguments the length and width of the user's rectangle and returns its surface.
+function calcSurface(l,w) {
+    return l * w;
+}
+
+console.log("The surface of your rectangle is " + calcSurface(l,w) + " (the unit of your choice)²")
