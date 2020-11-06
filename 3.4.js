bab87f40ad01818191700aa5fda5383477104af0
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-06 15:38:52 +0100

    Exercise 3.4

---
+++ 3.4.js
@@ -0,0 +1,9 @@
+// Program displaying the minimum and maximum of given array
+
+let testArr = [1, 2, 10, 3, 4, 5];
+let testArr2 = [100, 101, 102, 2285, 485];
+
+let max = Math.max(...testArr2);
+let min = Math.min(...testArr2);
+
+console.log("This arrays/' minmum value is " + min + " and its maximum value is " + max + ".");
