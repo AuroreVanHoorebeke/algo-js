26a6fa82078d3fbd26dbe4dd116af0e9b7c14c16
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-04 10:13:16 +0100

    exercise 4.6

--- 4.6.js
+++ 4.6.js
@@ -1,28 +1,28 @@
 /*Aim:  Creating a recursive function called "factorial(a)" returning the factorial of a number.
 Ex: 6! = 1*2*3*4*5*6 //720
-! Should be undifined for negative numbers and 0! = 1.*/
+! Should be undefined for negative numbers and 0! = 1.*/
 
 const readlineSync = require('readline-sync');
 
 console.log("This program will help you calculate the factorial of a number.");
-let num = Number(readlineSync.question("Type a number: "));
-let a = [];
+let a = Number(readlineSync.question("Type a number: "));
 
-function arrayPush(num) {
+let multiplication= 1;
 
-    for (let i = 0; i < num; i++) {
-        a.push(i + 1);
-    }
-    return a;
-}
-
+/**
+ * Calculates the factorial of a number.
+ */
 function factorial(a) {
-     arrayPush(num);
-     let result = 1;
-     for (i = 0; i < a.length; i++) {
-         result *= a[i];
-     }
-     return result;
- }
 
-console.log(`The factorial of ${num} is ${factorial(a)}.`);
+    multiplication*=a;
+
+    a--;
+
+    if(a>1){
+    factorial(a);
+    }
+    return multiplication;
+}
+let factorialFinal=factorial(a);
+
+console.log(`The factorial of ${a} is ${factorialFinal}.`);
